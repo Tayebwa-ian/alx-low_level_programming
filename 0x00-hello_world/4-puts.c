130cdc9464da3ac3a3c763233ff5f5f921f8bd4d
root <tayebwaian0@gmail.com>
2023-07-13 16:41:25 -0700

    c program using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 /**
 *main - program entry point
-*return : always 0 (success)
+*return: always 0 (success)
 */
 int main(void)
 {
-  /* Function description */
 char nm[52] = "\"Programming is like building a multilingual puzzle";
 puts(nm);
 return (0);
