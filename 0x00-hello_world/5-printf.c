95d3bb6d37746421c44b459661802362d245dd2b
root <tayebwaian0@gmail.com>
2023-07-13 17:45:57 -0700

    Use printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 /**
- *
  * main - program entry point
  *
  * Description: 'Using printf c function'
@@ -9,6 +8,6 @@
  */
 int main(void)
 {
-  printf ("with proper grammar, but the outcome is a piece of art,");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
