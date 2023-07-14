69b8913f8d962fb7fbb7538136b1b423ee7149f7
root <tayebwaian0@gmail.com>
2023-07-13 17:13:58 -0700

    Uses puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
  * main - Entry point
- * 
+ *
  * Description: 'Using puts function'
  * @parameter:this function doesnt take any parameters
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
