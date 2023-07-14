5f1f57efb999b20ec0fb42d094d3d822f3918810
root <tayebwaian0@gmail.com>
2023-07-13 19:53:06 -0700

    print to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'A program that writes to the standard error'
+ *
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
