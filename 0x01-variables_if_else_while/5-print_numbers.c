ab7802b20c1e824b21817d0fef43d45c027a3f88
root <tayebwaian0@gmail.com>
2023-07-16 12:55:56 -0700

    printing base 10 digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Description: "printing base 10 digits"
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int n = 0;
+while (n < 10)
+{
+printf("%d", n);
+n++;
+}
+printf("\n");
+return (0);
+}
