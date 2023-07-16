a117ca032d16b44311441c1ec00c7716f77353fe
root <tayebwaian0@gmail.com>
2023-07-16 12:45:16 -0700

    Skip some letters while printing alphabetic letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Description: "Skip some letter in the alphabet"
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch<='z'; ch++)
+{
+if (ch != 'q' && ch != 'e')
+{
+putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}
