8508ab72d9162a7b7f0a14351113f68c7eb34591
root <tayebwaian0@gmail.com>
2023-07-17 04:15:49 -0700

    combination of 4 digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,12 +9,12 @@ int main(void)
 int n, m;
 for (n = 0; n <= 98; n++)
 {
-for (m = n + 1; m < 99; m++)
+for (m = n + 1; m <= 99; m++)
 {
 putchar('0' + (n / 10));
-putchar('0' + (m % 10));
+putchar('0' + (n % 10));
 putchar(' ');
-putchar('0' + (n / 10));
+putchar('0' + (m / 10));
 putchar('0' + (m % 10));
 if (n != 98 && m != 99)
 {
