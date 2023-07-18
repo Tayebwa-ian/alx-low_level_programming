b5d359c9a30329d020d715b47cc24cca691cc256
root <tayebwaian0@gmail.com>
2023-07-18 08:48:39 -0700

    finding absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+/**
+ * _abs - find absolute value of a number
+ *
+ * @n: the number to find absolute value for
+ *
+ * return: the absolute value
+ */
+int _abs(int n)
+{
+int value;
+if (n < 0)
+{
+value = n * -1;
+return (value);
+}
+value = n;
+return (value);
+}
