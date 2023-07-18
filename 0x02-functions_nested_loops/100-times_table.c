cbd4de18166a8e7850a5ba351d3c86da3ed952a4
root <tayebwaian0@gmail.com>
2023-07-18 13:49:12 -0700

    a multiplication table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * print_times_table - printing time table
+ *
+ * @n: table of integer n to be built
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+if (n < 0 || n > 15)
+{
+return;
+}
+else
+{
+int i, j, result;
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+result = i * j;
+if (j != n)
+{
+printf("%d, ", result);
+}
+else
+{
+printf("%d", result);
+}
+}
+printf("\n");
+}
+}
+}
