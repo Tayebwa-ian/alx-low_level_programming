caeed8a9cb1cbc436d4565dd44d7915773514a24
root <tayebwaian0@gmail.com>
2023-07-18 15:13:02 -0700

    a multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,13 +20,20 @@ for (i = 0; i <= n; i++)
 for (j = 0; j <= n; j++)
 {
 result = i * j;
+if (j > 0)
+{
 if (j != n)
 {
-printf("%4d, ", result);
+printf("%3d, ", result);
 }
 else
 {
-printf("%4d", result);
+printf("%3d", result);
+}
+}
+else
+{
+printf("%d, ", result);
 }
 }
 printf("\n");
