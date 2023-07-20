c10118f8bc0123dfbbab9953e884e72f82e4f288
root <tayebwaian0@gmail.com>
2023-07-20 08:40:31 -0700

    prints 0 to 9 excpet 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * print_numbers - print numbers
+ * print_most_numbers - print numbers
  *
  * Return: void
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 int i;
 for (i = 0; i < 10; i++)
