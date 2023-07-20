6fc5f49f2bb1a76b17f4c04830beebb2747e6571
root <tayebwaian0@gmail.com>
2023-07-20 07:10:00 -0700

    checks for a digit (0 through 9).

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _isupper - check digits
+ * _isdigit - check digits
  * @c: The digit to be checked
  * Return: 1 (Success) 0 (Failure)
  */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
 if (c > 47 && c < 58)
 {
