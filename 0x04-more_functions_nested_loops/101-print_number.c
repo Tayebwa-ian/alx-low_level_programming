cc63ea35301783b50ef49ee78228684cad933d4c
root <tayebwaian0@gmail.com>
2023-07-21 13:31:45 -0700

    last number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,5 +6,16 @@
  */
 void print_number(int n)
 {
-_putchar('0' + n);
+unsigned int num;
+num = n;
+if (n < 0)
+{
+_putchar('-');
+num = -num;
+}
+if ((num / 10))
+{
+print_number(num / 10);
+}
+_putchar('0' + num % 10);
 }
