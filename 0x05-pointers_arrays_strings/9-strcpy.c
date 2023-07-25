edebc85b5684f23018a5a7fc5665b507a6be6054
root <tayebwaian0@gmail.com>
2023-07-25 05:46:22 -0700

    copy string using pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,11 @@
+/**
+ *_strcpy - copy a string
+ *@src: string varable to copy from
+ *@dest: string varable to copy to
+ *Return: Address of copies string
+ */
+char *_strcpy(char *dest, char *src)
+{
+dest = src;
+return (dest);
+}
