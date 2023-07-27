59afa5059dc1fdfa09d76d52187098922abac45c
root <tayebwaian0@gmail.com>
2023-07-27 08:37:52 -0700

    reverse an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,21 +9,21 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i, *last_address, *first_address;
+	int i, *last_address, *first_address, temp;
 
 	i = 0;
 	first_address = a;
 	last_address = a + (n - 1);
 	while (i < n)
 	{
-		if (*last_address == *first_address)
+		if (first_address < last_address)
 		{
-			printf("%d", *last_address);
+			temp = *a;
+			*a = *last_address;
+			*last_address = temp;
 		}
-		else
-			printf("%d, ", *last_address);
 		i++;
+		a++;
 		last_address--;
 	}
-	printf("\n");
 }
