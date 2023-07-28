3dc01eaa960b304aa9d69b8736dcd8369cac85fc
root <tayebwaian0@gmail.com>
2023-07-28 05:16:38 -0700

    print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,19 +1,38 @@
 #include "main.h"
+
 /**
  *print_number - print a number
  *@n: the number
  *Return: void
  */
+
 void print_number(int n)
 {
-	if (n < 10)
+	char bucket[20];
+	int i, j;
+
+	/* Handles the case for when n is a negative */
+	if (n < 0)
 	{
-		n = n + 48;
-		_putchar('0' + n);
+		_putchar('-');
+		n = -n; /* turn n into positive number */
 	}
+
+	if (n < 10)
+		_putchar('0' + n);
 	else
-		while (n >= 0)
+	{
+		i = 0;
+		while (n != 0)
 		{
+			bucket[i] = n % 10 + '0';
 			n = n / 10;
-			
+			i++;
+		}
+		bucket[i] = '\0';
+		for (j = i; j >= 0; j--)
+		{
+			_putchar(bucket[j]);
+		}
+	}
 }
