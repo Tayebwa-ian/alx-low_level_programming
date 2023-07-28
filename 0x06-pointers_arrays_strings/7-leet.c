0e762da636e1c88af0231e1fcd6741ff83aa3c6f
root <tayebwaian0@gmail.com>
2023-07-27 17:34:59 -0700

    encode

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,20 +5,22 @@
  */
 char *leet(char *str)
 {
+	int i, j;
 	char *first_address = str;
+	char ch[10] = "AaEeOoTtLl";
+	char dig[10] = "4433007711";
 
 	while (*str != '\0')
 	{
-		if (*str == 65 || *str == 97)
-			*str = 52;
-		else if (*str == 101 || *str == 69)
-			*str = 51;
-		else if (*str == 111 || *str == 79)
-			*str = 48;
-		else if (*str == 84 || *str == 116)
-			*str = 55;
-		else if (*str == 76 || *str == 108)
-			*str = 49;
+		j = 0;
+		i = 0;
+		while (i < 10)
+		{
+			if (ch[i] == *str)
+				*str = dig[j];
+			i++;
+			j++;
+		}
 		str++;
 	}
 	return (first_address);
