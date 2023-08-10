1b7d029ee921bf0ba3856b35e6f5218929b18bde
Tayebwa-ian <tayebwaian0@gmail.com>
2023-08-10 16:33:36 -0700

    join two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,9 +29,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0, j = 0;
 
 	if (s1 == NULL)
-		s1 = "";
+		i = 0;
 	if (s2 == NULL)
-		s2 = "";
+		j = 0;
 
 	if (n == _strlen(s2) || n > _strlen(s2))
 	{
