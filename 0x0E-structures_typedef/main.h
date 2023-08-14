228522796024efc4b4b6fe25f014de3e9b95a066
Tayebwa-ian <tayebwaian0@gmail.com>
2023-08-14 07:17:11 -0700

    create dog struct

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,16 +8,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-/**
- * struct dog - Struct with dog elements
- * @age: age of the dog
- * @name: name of the dog
- * @owner: owner of the dog
- */
-struct dog
-{
-	char *name;
-	char *owner;
-	float age;
-};
-#endif /* DOG_H */
+#endif /* MAIN_H */
