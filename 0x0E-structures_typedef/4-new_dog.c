de71e171224936b2dda83f58dbfa8b5ed237d9a0
Tayebwa-ian <tayebwaian0@gmail.com>
2023-08-14 12:04:55 -0700

    creates a new dog.

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ *_strlen - cal the length of a string
+ *@str: the string
+ *Return: length of a string
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+		i++;
+	return (i);
+}
+/**
+ *new_dog - creates a new dog
+ *@name: name of the new dog
+ *@age: age of a new dog
+ *@owner: owner of the new dog
+ *Return: Pointer to the new dog struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i = 0, j = 0;
+	dog_t *new;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	new = malloc(sizeof(*new));
+	if (!new)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->name = malloc(_strlen(name) + 1);
+	if (!(new->name))
+	{
+		free(new->name);
+		return (NULL);
+	}
+	while (*(name + i) != '\0')
+	{
+		new->name[i] = *(name + i);
+		i++;
+	}
+	new->name[i] = '\0';
+	new->owner = malloc(_strlen(owner) + 1);
+	if (!(new->owner))
+	{
+		free(new->owner);
+		return (NULL);
+	}
+	while (*(owner + j) != '\0')
+	{
+		new->owner[j] = *(owner + j);
+		j++;
+	}
+	new->owner[j] = '\0';
+	new->age = age;
+	return (new);
+}
