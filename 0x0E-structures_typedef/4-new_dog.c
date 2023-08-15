9e8a471f239798fb2d6de9ce2ce2e2eb0e3f3990
Tayebwa-ian <tayebwaian0@gmail.com>
2023-08-15 03:37:22 -0700

    create a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -31,7 +31,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	new = malloc(sizeof(*new));
 	if (new == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 	new->name = malloc(_strlen(name) + 1);
 	if (new->name == NULL)
 	{
