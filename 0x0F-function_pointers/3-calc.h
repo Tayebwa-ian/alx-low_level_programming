7b02f234428022307bb748dc4780e979b63e80b6
Tayebwa-ian <tayebwaian0@gmail.com>
2023-08-16 15:18:49 -0700

    performs simple operations

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,8 +1,9 @@
 #ifndef OPERATIONS_H
-#define OPERATION_H
+#define OPERATIONS_H
 #include <stdlib.h>
 #include <stdio.h>
 #include <stddef.h>
+#include <string.h>
 /**
  * struct op - Struct op
  *
@@ -11,8 +12,8 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,29 +1,28 @@
 #include "3-calc.h"
 /**
  *main - performs simple operations of math
- *@arg: name of the file
  *@argc: number of arguments passed
  *@args: pointer to array of arguments
  *Return: result or error
  */
-int main(int argc, char* args[])
+int main(int argc, char *args[])
 {
-	int result, a, b, c;
+	int result, a, b;
 
 	a = atoi(args[1]);
-	c = atoi(args[2]);
 	b = atoi(args[3]);
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
 	if (get_op_func(args[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((c == '/' || c == '%') && b == 0)
+	if ((strcmp(args[2], "/") == 0 || strcmp(args[2], "%") == 0) && b == 0)
 	{
 		printf("Error\n");
 		exit(100);
