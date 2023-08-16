9d49fa70a14a2415daf9f14e57a99b773666abec
Tayebwa-ian <tayebwaian0@gmail.com>
2023-08-16 14:22:50 -0700

    performs simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ *op_add - add two integers
+ *@a: first integer
+ *@b: second integer
+ *Return: result of the operation
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - subtract 2 integers
+ *@a: first integer
+ *@b: second integer
+ *Return: result of the operation
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - multiply two integers
+ *@a: first integer
+ *@b: second integer
+ *Return: result of the operation
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - divide two integers
+ *@a: first integer
+ *@b: second integer
+ *Return: result of the operation
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - find the module of two integers
+ *@a: first integer
+ *@b: second integer
+ *Return: result of the operation
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
