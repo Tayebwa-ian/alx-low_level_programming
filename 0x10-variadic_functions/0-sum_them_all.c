8e17aaf431eb339f552beca10b62aa76ce7124e7
Tayebwa-ian <tayebwaian0@gmail.com>
2023-08-17 06:09:23 -0700

    find sum of unknown arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 /**
  *sum_them_all -  find sum of the unknown arguments
- *@n - number of unknown arguments
+ *@n: number of unknown arguments
  *Return: sum or 0
  */
 int sum_them_all(const unsigned int n, ...)
