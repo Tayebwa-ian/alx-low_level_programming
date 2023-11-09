9dccdd3dfdf3541cd74f4d19ec437070e95fd98b
Tayebwa-ian <tayebwaian0@gmail.com>
2023-11-09 04:43:31 -0800

    add node at any specified index in a list

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ *insert_dnodeint_at_index - insert a node at a specified index in a list
+ *@h: head of the list
+ *@idx: index to insert the new node
+ *@n: data element of the new node
+ *Return: the address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int c = 0;
+	dlistint_t *start, *new, *temp, *last;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	start = *h;
+	while (start)
+	{
+		c += 1;
+		if (c == idx && start->next == NULL)
+		{
+			free(new);
+			last = add_dnodeint_end(h, n);
+			while (last)
+				last = last->next;
+			return (last);
+		}
+		else if (c == idx)
+		{
+			temp = start->next;
+			start->next = new;
+			new->prev = start;
+			new->next = temp;
+			return (new);
+		}
+		start = start->next;
+	}
+	return (NULL);
+}
