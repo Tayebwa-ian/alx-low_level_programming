4781c18e05a48b02d77c42d5e225fc400bf773c2
Tayebwa-ian <tayebwaian0@gmail.com>
2024-03-13 23:18:56 +0300

    Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -18,18 +18,19 @@ int jump_search(int *array, size_t size, int value)
 		blocksize = sqrt(size);
 		start = 0;
 		end = blocksize;
-		while (*(array + end) <= value && end < size)
+		printf("Value checked array[%ld] = [%d]\n", start, *(array + start));
+		while (*(array + end) < value && end < size)
 		{
-			printf("Value checked array[%ld] = [%d]\n", start, *(array + start));
 			start = end;
 			end = end + blocksize;
-			if (end > size - 1)
-				end = size;
+			printf("Value checked array[%ld] = [%d]\n", start, *(array + start));
 		}
 		printf("Value found between indexes [%ld] and [%ld]\n", start, end);
 		/* perform linear search on a subarray */
-		for (i = start; i < end; i++)
+		for (i = start; i <= end; i++)
 		{
+			if (i == size)
+				break;
 			printf("Value checked array[%ld] = [%d]\n", i, *(array + i));
 			if (*(array + i) == value)
 				return (i);
