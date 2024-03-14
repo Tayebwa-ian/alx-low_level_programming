6e1cf1db7fef7085321dd786fc873317bce02421
Tayebwa-ian <tayebwaian0@gmail.com>
2024-03-14 12:07:51 +0300

    fix Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -30,10 +30,10 @@ int jump_search(int *array, size_t size, int value)
 		for (i = start; i <= end; i++)
 		{
 			printf("Value checked array[%ld] = [%d]\n", i, *(array + i));
-			if (i == size - 1)
-				break;
 			if (*(array + i) == value)
 				return (i);
+			if (i == size - 1)
+				break;
 		}
 	}
 	return (-1);
